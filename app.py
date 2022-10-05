0798c900d6634c34806508c678fae35dcb4c6975
Johnny Bailey <109354592+jbailey2705@users.noreply.github.com>
2022-10-05 10:29:35 -0500

    adding Weather_app

--- app.py
+++ app.py
@@ -4,5 +4,11 @@ app = Flask(__name__)
 def Hello_World():
     return 'Hello World'
 
+# for the above code, in terminal use ~ export FLASK_APP=app.py (enter) then flask run
+
+# if you use the below code you will need to add the following to terminal ~ python app.py
 if __name__ == '__main__':
     app.run()
+
+# http://127.0.0.1:5000/
+
